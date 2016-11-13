5302e40808f08de4edb243afde4c728e7089aa54
yangguangyu <xxgcxy123@163.com>
2016-11-13 16:09:08 +0800

    Initial Commit

---
+++ GYMarquee/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GYMarquee
+//
+//  Created by yangguangyu on 16/11/13.
+//  Copyright © 2016年 yangguangyu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ GYMarquee/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  GYMarquee
+//
+//  Created by yangguangyu on 16/11/13.
+//  Copyright © 2016年 yangguangyu. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
